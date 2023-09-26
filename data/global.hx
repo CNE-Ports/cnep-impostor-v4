454da3c834ecd17a6bd158df2df2462fcdb25945
swordcube <swordsoftarchive@gmail.com>
2023-09-25 20:25:41 -0500

    yce like logs system made it to allow debugging without a terminal
    Co-authored-by: xavi. <ThatOneIdiotXav@users.noreply.github.com>

---
+++ data/global.hx
@@ -0,0 +1,31 @@
+import funkin.backend.scripting.Script;
+
+var redirectStates:Map<FlxState, String> = [
+    MainMenuState => "impostor/MainMenu",
+];
+
+function preStateSwitch() {
+    for(redirectState in redirectStates.keys()) {
+        if(Std.isOfType(FlxG.game._requestedState, redirectState)) 
+            FlxG.game._requestedState = new ModState(redirectStates.get(redirectState));
+    }
+}
+
+var logsScript:Script = Script.create(Paths.script("data/modules/LogsOverlay"));
+logsScript.load();
+logsScript.call("create", []);
+
+function postUpdate(delta:Float) {
+    if(FlxG.keys.justPressed.F5)
+        FlxG.resetState();
+    
+    if(FlxG.keys.justPressed.F6)
+        logsScript.call("toggle", []);
+
+    logsScript.update(delta);
+}
+
+function onDestroy() {
+    logsScript.call("onDestroy", []);
+    logsScript.destroy();
+}
