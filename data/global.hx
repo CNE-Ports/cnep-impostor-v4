91b09454621f3badf100ce038484e802a0eedf39
swordcube <swordsoftarchive@gmail.com>
2023-09-26 13:16:49 -0500

    log system touchups

--- data/global.hx
+++ data/global.hx
@@ -4,6 +4,10 @@ var redirectStates:Map<FlxState, String> = [
     MainMenuState => "impostor/MainMenu",
 ];
 
+var logsScript:Script = Script.create(Paths.script("data/modules/LogsOverlay"));
+logsScript.load();
+logsScript.call("create", []);
+
 function preStateSwitch() {
     for(redirectState in redirectStates.keys()) {
         if(Std.isOfType(FlxG.game._requestedState, redirectState)) 
@@ -11,9 +15,9 @@ function preStateSwitch() {
     }
 }
 
-var logsScript:Script = Script.create(Paths.script("data/modules/LogsOverlay"));
-logsScript.load();
-logsScript.call("create", []);
+function postStateSwitch() {
+    logsScript.call("postStateSwitch", []);
+}
 
 function postUpdate(delta:Float) {
     if(FlxG.keys.justPressed.F5)
